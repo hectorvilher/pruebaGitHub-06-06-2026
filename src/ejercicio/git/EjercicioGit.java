f3a7fa9ba20209efbb4167e3569ff9d00709d8ca
Hector Villar <bon_hector85@hotmail.com>
2026-06-06 11:04:05 +0200

    ver. inicial

---
+++ src/ejercicio/git/EjercicioGit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package ejercicio.git;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Hector
+ */
+public class EjercicioGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+    }
+
+}
